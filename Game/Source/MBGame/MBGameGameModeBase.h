74bdbb1b39cbe388d5a215f0e698eb7967340aff
MelvinBrink <48125702+MelvinBrink@users.noreply.github.com>
2023-03-26 21:15:02 +0200

    Initial Commit

---
+++ Game/Source/MBGame/MBGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MBGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MBGAME_API AMBGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
